8da9af4db8eb1284246213aabdcba69d349e049c
Bruno de A. Bisogni <bruno@sociodotabuleiro.fun>
2024-07-12 14:48:26 -0300

    arquivo confirm-trip
    Arquivo confirm-trip com problema no ultimo if do reply.

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -1,18 +1,46 @@
 import type { FastifyInstance }  from 'fastify';
 import type { ZodTypeProvider} from "fastify-type-provider-zod";
 import { z } from 'zod';
+import { prisma } from '../lib/prisma';
+
+
+
+
+
 
 
 export async function confirmTrip(app: FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>().post('/trips:tripId/confirm', {
-     
-      schema:{
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/trips/:tripId/confirm', 
+    {
+      schema: {
         params: z.object({
-          tripId: z.string().uuid(),
-        })
-      }
-    }, async (request)=> {
-      return  { tripId : request.params.tripId } 
-    }) 
-}
+          tripId: z.string().uuid(), 
+        }),
+      },
+    }, 
+    async (resquest) => {
+      const {tripId} = resquest.params
 
+      const trip = await prisma.trip.findUnique({
+        where: {
+          id: tripId, 
+        }
+      })
+        if (!trip) {
+          throw new Error('Viagem não encontrada')
+        }
+
+        if (trip.is_confirmed) {
+          throw new Error('Viagem já confirmada')
+        } 
+
+        if (trip.is_confirmed){
+          return reply.redirect(`htpps://localhost:3333/trip/${tripId}`)
+        }
+
+        return { tripId: resquest.params.tripId}
+    }
+        
+  )
+}
